0d62e9dbc6851451c6b289c1df23d23c46102931
xiangxin <xiangxin.ji@smartx.com>
2025-08-05 18:27:19 +0800

    fix: SaveButton loading prop type

--- src/components/theme/components/form.tsx
+++ src/components/theme/components/form.tsx
@@ -121,7 +121,7 @@ export const Form = <
 
             <SaveButton
               type="submit"
-              loading={props.refineCore.formLoading}
+              loading={props.refineCore.formLoading.toString()}
               {...saveButtonProps}
             />
           </div>
